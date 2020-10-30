306bbd4b20343582b230a953b25b91e0df4000dd
kibria7533 <tenminuteversity@gmail.com>
2020-10-30 15:31:13 +0600

    first commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose');
 const cor=require('cors')
 
 app.use(bp.json());
-mongoose.connect('mongodb://localhost:27017/test',{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect(process.env.DB_URL||'mongodb://localhost:27017/test',{useNewUrlParser:true,useUnifiedTopology:true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
