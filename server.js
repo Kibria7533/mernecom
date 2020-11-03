9735fee5148ef42cfb92ced8f525177c8b5b578d
kibria7533 <tenminuteversity@gmail.com>
2020-11-04 00:15:37 +0600

    1st

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cor=require('cors')
 
 app.use(bp.json());
 // process.env.DB_URL||||'mongodb://localhost:27017/test'
-mongoose.connect('mongodb://localhost:27017/test' ,{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect(process.env.DB_URL ,{useNewUrlParser:true,useUnifiedTopology:true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
